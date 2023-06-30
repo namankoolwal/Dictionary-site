39ed7389e71e60aa1d738738fc03fc7f50905eee
Naman khandelwal <namankoolwal1411@gmail.com>
2023-06-30 13:54:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Route,Routes } from 'react-router-dom';
-
 import Dictionary from "./component/Dictionary";
 import Navbar from './component/Navbar';
 import Footer from './component/Footer';
@@ -8,9 +7,6 @@ import About from './component/About';
 import Contact from './component/Contact';
 import Login from './component/Login';
 import Signup from './component/Signup';
-// import { GoogleOAuthProvider } from '@react-oauth/google';
-// import { GoogleLogin } from '@react-oauth/google';
-
 
 function App() {
   return (
@@ -23,18 +19,6 @@ function App() {
       <Route exact path="/login" element={<Login/>}/>
       <Route exact path="/register" element={<Signup/>}/>
     </Routes>
-
-{/* <GoogleOAuthProvider clientId="78940162672-otnijmqjlr47tj63g4sske1ef13dovqi.apps.googleusercontent.com">
-
-<GoogleLogin
-  onSuccess={credentialResponse => {
-    console.log(credentialResponse);
-  }}
-  onError={() => {
-    console.log('Login Failed');
-  }}
-/>
-</GoogleOAuthProvider> */}
      <Footer/>
     </>
   );
