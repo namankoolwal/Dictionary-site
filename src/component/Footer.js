dff97a104470ea599ccb86e649dab9d9040e82dd
Naman khandelwal <namankoolwal1411@gmail.com>
2023-07-03 19:41:15 +0530

    commit

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,56 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      {/* <div className="footer">
-       <div className="flex-column justify-content-bottom">
-       <div className="navbar navbar-expand-lg navbar-light bg-white py-1 shadow-lg ">
-         <div className=" navbar-nav mx-auto mb-2 mb-lg-0 " id="navbarSupportedContent">
-           <div className="navbar-nav" >
-             <h5>
-             
-               <i className="fa fa-copyright me-5"> 2023 NAMAN KHANDELWAL</i>
-             </h5>   */}
 
-
-
-      <footer class="page-footer font-small blue">
-
-        <div class="footer-copyright text-center py-1 shadow-lg">© 2023 NAMAN KHANDELWAL
-          {/* <a href="/"> MDBootstrap.com</a> */}
+      <footer className="page-footer font-small fixed-bottom bg-white ">
+        <div className="footer-copyright text-center py-1 shadow-lg sticky-bottom">© 2023 NAMAN KHANDELWAL
         </div>
-
       </footer>
-
-
-
-      {/* <div className="nav-item " id="navbarSupportedContent">
-               <a href="https://instagram.com/naman_koolwal"> 
-                 <i className="fa fa-instagram me-4"> Instagram</i>
-               </a>
-               <a href="https://www.facebook.com/naman.koolwal"> 
-                 <i className="fa fa-facebook me-4"> Facebook</i>
-               </a>
-               <a href="https://twitter.com/NAMANKH31936926?t=NgPyoRrpwAqAYonRRIUXxA&s=08"> 
-                 <i className="fa fa-twitter me-4"> Twitter</i>
-               </a>
-               <a href="https://github.com/namankoolwal">
-                 <i className="fa fa-github me-4"> Github</i>
-               </a>
-               <a href="https://www.linkedin.com/in/naman-khandelwal-568971189/">
-                 <i className="fa fa-linkedin me-4"> Linkedin</i>
-               </a>
-               <a href="mailto:namankoolwal1411@gmail.com">
-                 <i className="fa fa-envelope me-4"> E-Mail</i>
-               </a>
-             </div> */}
-
-
-
-      {/* </div>
-         </div>
-       </div>
-     </div>
-     </div> */}
     </>
   );
 };
